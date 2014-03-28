f546c25272460720c1bee7feb954f13d249b715f
SW4 <samuel.wheeler@gmail.com>
2014-03-28 13:22:56 +0000

    update 0.0.1

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var ngGa = angular.module('ngGa', []);
+ngGa.user="sw4";
+ngGa.controller("gitListController", ['$scope', 'gitFactory', '$timeout', function ($scope, gitFactory, $timeout) {
+    $scope.getRepos= function(user){
+        gitFactory.fetch(user, 'repos').then(function (response) {        
+            var repos=_(response.data.data).sortBy(function(obj) { return obj.updated_at }).reverse();
+            angular.forEach(repos, function(repo){
+                repo.timeago=$.timeago(repo.updated_at);
+            });
+            $timeout(function(){
+                $scope.repos=repos;
+            });                
+        });
+    }    
+}]);
+ngGa.factory('gitFactory', ['$http', '$timeout', function ($http, $timeout) {
+    var factory = {
+        fetch : function (user, type) {
+            return $http.jsonp('https://api.github.com/users/'+user+'/'+type+'?callback=JSON_CALLBACK').success(function (data) {
+                return data;
+            }).error(function (e) {
+                return e;
+            });
+        }
+    };
+    return factory;    
+}]);
